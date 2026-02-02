2c21371adb815b68a957cb856b261dd327200c25
Lawrence Arryl Lopez <160656561+arryllopez@users.noreply.github.com>
2026-02-01 20:56:28 -0500

    fixed the disgusting gradient cutoff

--- frontend/components/ui/gradient-backgrounds.tsx
+++ frontend/components/ui/gradient-backgrounds.tsx
@@ -13,7 +13,7 @@ export function GradientBackground({
     <div
       className="absolute inset-0 z-0"
       style={{
-        background: `radial-gradient(125% 125% at 50% 90%, ${fromColor} 50%, ${toColor} 100%)`,
+        background: `radial-gradient(125% 125% at 50% 70%, ${fromColor} 50%, ${toColor} 100%)`,
       }}
     />
   );
